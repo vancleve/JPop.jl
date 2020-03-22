72f297f609e33671168160fb8b55452b34980151
Daniel <dpriego87@gmail.com>
2020-03-21 22:38:01 -0400

    Distributions and LinearAlgebra libraries added

--- src/JPop.jl
+++ src/JPop.jl
@@ -1,4 +1,11 @@
+# Simulation of population dynamics
+#
+# Jeremy Van Cleve <jeremy.vancleve@gmail.com>
+# Daniel Priego Espinosa <dpriego87@gmail.com>
+
 module JPop
+
+using Distributions, LinearAlgebra
 
 greet() = print("Hello World!")
 
