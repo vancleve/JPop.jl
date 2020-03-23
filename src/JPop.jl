1a01882072ec979ef57253d3ff0d447e1eee545e
Daniel <dpriego87@gmail.com>
2020-03-22 23:22:28 -0400

    Main modules added

--- src/JPop.jl
+++ src/JPop.jl
@@ -7,5 +7,21 @@ module JPop
 
 using Distributions, LinearAlgebra
 
+export
+# Basic types
+Individual, Population,
+# population quantities
+mean_genotype, mean_phenotype, age_distribution,
+# population dynamics
+next_gen!
+
+include("PopStructures/PopStructures.jl")
+using .PopStructures
+include("PopQuantities/PopQuantities.jl")
+using .PopQuantities
+include("EnvDynamics/EnvDynamics.jl")
+using .EnvDynamics
+include("PopDynamics/PopDynamics.jl")
+using .PopDynamics
 
 end # module
