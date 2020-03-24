944d567e90de225ece95912ea55a6cba0224d8ef
Daniel <dpriego87@gmail.com>
2020-03-24 00:34:17 -0400

    !-ending convention in function names to all functions that modify inputs

--- src/EnvDynamics/EnvDynamics.jl
+++ src/EnvDynamics/EnvDynamics.jl
@@ -1,7 +1,7 @@
 # Environment update functions
 module EnvDynamics
 using ..PopStructures: Population
-export update_env_state
+export update_env_state!
 # "autoregressive" type model
 function autoregressive(x::Array{Float64,1}, θ::Array{Float64,1}=[0.5], σ::Float64=1.0)
     x_new = zeros(size(x))
@@ -13,8 +13,8 @@ function autoregressive(x::Array{Float64,1}, θ::Array{Float64,1}=[0.5], σ::Flo
     return x_new
 end
 
-function update_env_state(pop::Population)
-    copy!(pop.env_state, pop.env_func(pop.env_state))
+function update_env_state!(pop::Population)
+    copy!(pop.env_state, pop.env_func!(pop.env_state))
     nothing
 end
 
