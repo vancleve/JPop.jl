7129bf26477301ca474ecb8cf15a7fa6536d77a8
Daniel <dpriego87@gmail.com>
2020-04-06 20:51:40 -0400

    geno_func renamed to geno_func0 to better indicate the expected functionality of geno_func, which is to set the  genotype assignment function for the initial population members

--- src/PopStructures/constructors/population.jl
+++ src/PopStructures/constructors/population.jl
@@ -18,12 +18,12 @@ mutable struct Population
     ## e.g., ()->[rand()] initializes each genotype to a random value in (0,1)
     function Population(size::Int,
                         pheno_func::Function, npheno::Int, fit_func::Function,
-                        mut_func::Function, geno_func::Function,
+                        mut_func::Function, geno_func0::Function,
                         env_func::Function, env0::Array{Float64,1})
 
         members = Array{Individual}(undef,size)
         for i=1:size
-            g = geno_func(i)
+            g = geno_func0(i)
             ind = Individual(0, g, Array{Float64}(undef,npheno))
             pheno_func(ind, env0)
             members[i] = ind
