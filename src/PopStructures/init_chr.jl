e6b0097eda864a4bec7a45ff4e6701424089708a
Daniel <dpriego87@gmail.com>
2020-04-24 22:21:40 -0400

    optional argument tag, it allows customize allele tag prefix

--- src/PopStructures/init_chr.jl
+++ src/PopStructures/init_chr.jl
@@ -1,8 +1,8 @@
-function set_genome!(i::HapIndividual, rec_rf::Function, rec_hat::Float64, mut_rf::Function, mu_hat::Float64)
+function set_genome!(i::HapIndividual, rec_rf::Function, rec_hat::Float64, mut_rf::Function, mu_hat::Float64; tag="A")
     n = i.nloci
     chr = Chromosome(n)
     for ni=1:n
-        chr.loci_ids[ni] = "X$ni"
+        chr.loci_ids[ni] = "$tag$ni"
     end
     chr.rec_rate .= rec_rf(n,rec_hat)
     chr.mu .= mut_rf(n,mu_hat)
@@ -10,12 +10,12 @@ function set_genome!(i::HapIndividual, rec_rf::Function, rec_hat::Float64, mut_r
     nothing
 end
 
-function set_genome!(i::DipIndividual, rec_rf::Function, rec_hat::Float64, mut_rf::Function, mu_hat::Float64)
+function set_genome!(i::DipIndividual, rec_rf::Function, rec_hat::Float64, mut_rf::Function, mu_hat::Float64; tag="A")
     n = i.nloci
     for nhap=1:2
         chr = Chromosome(n)
         for ni=1:n
-            chr.loci_ids[ni] = "X$ni"
+            chr.loci_ids[ni] = "$tag$ni"
         end
         chr.rec_rate .= rec_rf(n,rec_hat)
         chr.mu .= mut_rf(n,mu_hat)
